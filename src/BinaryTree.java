73e97d91cba8d14ec413351cbef47b20ad108b79
liyuanhua <liyuanhua0512@126.com>
2016-03-31 20:25:56 +0800

    binary tree first

---
+++ src/BinaryTree.java
@@ -0,0 +1,84 @@
+/**
+ * Created by liyuanhua on 16/3/31.
+ */
+public class BinaryTree {
+
+    public static class Node {
+        int data;
+        Node leftChild;
+        Node rightChild;
+
+        Node(int data) {
+            this.data = data;
+            this.leftChild = null;
+            this.rightChild = null;
+        }
+    }
+
+        public Node insert(Node node, int data){
+            if(node == null){
+                node = new Node(data);
+            }
+            else{
+                if(data <= node.data){
+                    //System.out.println("insert");
+                    node.leftChild = insert(node.leftChild,data);
+                }
+                else if(data > node.data){
+                    //System.out.println("insert");
+                    node.rightChild =insert(node.rightChild,data);
+                }
+
+            }
+            return node;
+        }
+
+        public void show(Node node){
+
+            if(node == null){
+                System.out.println("show null");
+                return;
+            }
+            System.out.println(node.data);
+
+
+        }
+
+        public void preTravels(Node node){
+
+            //System.out.println("start once..");
+
+            if(node == null){
+                System.out.println("root is null");
+                return ;
+            }
+            else{
+                show(node);     //root node data first
+                preTravels(node.leftChild);
+                preTravels(node.rightChild);
+
+            }
+
+        }
+
+
+
+
+
+
+    public static void main(String[] args){
+
+
+        BinaryTree bt = new BinaryTree();
+
+
+        Node root = new Node(100);
+        root = bt.insert(root,111);
+        root = bt.insert(root,222);
+        root = bt.insert(root,999);
+        root = bt.insert(root,1);
+        bt.preTravels(root);
+    }
+
+
+}
